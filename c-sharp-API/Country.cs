057b3d55b561ba491cb1f6911e8f6141b7f60750
omranali2111 <aljabri1234@hotmail.com>
2023-07-25 13:49:58 +0400

    API

---
+++ c-sharp-API/Country.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace c_sharp_API
+{
+    internal class Country
+    {
+       
+            public string officialName { get; set; }
+            public string capital { get; set; }
+            public double area { get; set; }
+
+        public Country(string officialName, string capital, double area) 
+        { 
+            this.officialName = officialName;
+            this.capital = capital;
+            this.area = area;
+
+        }
+        public override string ToString()
+        {
+            return $"Name: {officialName}, capital: {capital}, area: {area}";
+        }
+
+
+    }
+}
---
+++ c-sharp-API/Program.cs
@@ -0,0 +1,65 @@
+﻿using c_sharp_API;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        List<Country> response = await CallAPI();
+        foreach (Country country in response)
+        {
+            Console.WriteLine(country);
+           
+        }
+    }
+     public  async static Task<List<Country>> CallAPI()
+    {
+        string API = "https://restcountries.com/v3.1/all?fields=name,capital,area";
+        HttpClient httpClient = new HttpClient();
+        HttpResponseMessage ResponseMessage = await httpClient.GetAsync(API);
+        List<Country> CountryList = new List<Country>();
+
+        if (ResponseMessage.IsSuccessStatusCode)
+        {
+            string stringRes=await ResponseMessage.Content.ReadAsStringAsync();
+            JsonDocument document=JsonDocument.Parse(stringRes);
+            JsonElement root= document.RootElement;
+            if(root.ValueKind == JsonValueKind.Array)
+            {
+                var enumerator = root.EnumerateArray;
+                foreach (JsonElement country in root.EnumerateArray())
+                {
+                    String capital="";
+                    if(country.GetProperty("capital").GetArrayLength() > 0)
+                    {
+                        capital = country.GetProperty("capital")[0].GetString();
+                    }
+
+                    double area=country.GetProperty("area").GetDouble();
+                    string officialName=country.GetProperty("name").GetProperty("official").GetString();
+
+                    CountryList.Add(new Country(officialName, capital, area));
+
+                }
+            }
+
+
+            
+
+           
+        }
+        return CountryList;
+
+
+     }
+   
+
+}
+    
